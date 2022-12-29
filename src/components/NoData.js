967f140b68b92b4421f320125742e4f09b38877f
imjoymhnt <mritunjoy.mahanta001@gmail.com>
2022-12-29 20:19:49 +0530

    unnecessary code remove

--- src/components/NoData.js
+++ src/components/NoData.js
@@ -1,6 +1,5 @@
-import { Button, Col, Empty, Row } from "antd";
+import { Col, Empty, Row } from "antd";
 import React from "react";
-import { Link } from "react-router-dom";
 import LinkButton from "./LinkButton";
 
 const NoData = () => {
